2daf37a2fe1fc48bc58ece2fa15c447b7a582a8d
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2024-07-25 12:17:29 +0700

    chors: change config url axios

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import config from "./config";
 
 const instance = axios.create({
-  baseURL: config.API_URL,
+  baseURL: config.API_HOSTING,
 });
 
 export default instance;
--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 // config.js
 const API_URL = "https://localhost:7059";
+const API_HOSTING = "https://notaion.somee.com";
 
 export default {
   API_URL,
+  API_HOSTING,
 };
