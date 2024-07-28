e45d854a2c4fb9ee109bec0eb8f2b30e3a1fd9ff
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2024-07-28 13:49:51 +0700

    feat: bubble menu tiptap

--- src/components/layouts/MenuBar/MenuBar.jsx
+++ src/components/layouts/MenuBar/MenuBar.jsx
@@ -1,20 +1,140 @@
 import "./MenuBar.scss";
-
+import * as Y from "yjs";
 import { Color } from "@tiptap/extension-color";
 import ListItem from "@tiptap/extension-list-item";
 import TextStyle from "@tiptap/extension-text-style";
+import Collaboration from "@tiptap/extension-collaboration";
+import CollaborationCursor from "@tiptap/extension-collaboration-cursor";
 import { EditorProvider, useCurrentEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-
+import { WebrtcProvider } from "y-webrtc";
+import { BubbleMenu, EditorContent } from "@tiptap/react";
+const ydoc = new Y.Doc();
+const provider = new WebrtcProvider("tic-tac-toe", ydoc);
 const MenuBar = () => {
   const { editor } = useCurrentEditor();
-
   if (!editor) {
     return null;
   }
 
   return (
     <div className="control-group">
+      {editor && (
+        <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
+          <div className="bubble-menu">
+            <button
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              className={editor.isActive("bold") ? "is-active" : ""}
+            >
+              Bold
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              className={editor.isActive("italic") ? "is-active" : ""}
+            >
+              Italic
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleStrike().run()}
+              className={editor.isActive("strike") ? "is-active" : ""}
+            >
+              Strike
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleCode().run()}
+              disabled={!editor.can().chain().focus().toggleCode().run()}
+              className={editor.isActive("code") ? "is-active" : ""}
+            >
+              Code
+            </button>
+            <button
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 1 }).run()
+              }
+              className={
+                editor.isActive("heading", { level: 1 }) ? "is-active" : ""
+              }
+            >
+              H1
+            </button>
+            <button
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 2 }).run()
+              }
+              className={
+                editor.isActive("heading", { level: 2 }) ? "is-active" : ""
+              }
+            >
+              H2
+            </button>
+            <button
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 3 }).run()
+              }
+              className={
+                editor.isActive("heading", { level: 3 }) ? "is-active" : ""
+              }
+            >
+              H3
+            </button>
+            <button
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 4 }).run()
+              }
+              className={
+                editor.isActive("heading", { level: 4 }) ? "is-active" : ""
+              }
+            >
+              H4
+            </button>
+            <button
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 5 }).run()
+              }
+              className={
+                editor.isActive("heading", { level: 5 }) ? "is-active" : ""
+              }
+            >
+              H5
+            </button>
+            <button
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 6 }).run()
+              }
+              className={
+                editor.isActive("heading", { level: 6 }) ? "is-active" : ""
+              }
+            >
+              H6
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleBulletList().run()}
+              className={editor.isActive("bulletList") ? "is-active" : ""}
+            >
+              Bullet list
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleOrderedList().run()}
+              className={editor.isActive("orderedList") ? "is-active" : ""}
+            >
+              Ordered list
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+              className={editor.isActive("codeBlock") ? "is-active" : ""}
+            >
+              Code block
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleBlockquote().run()}
+              className={editor.isActive("blockquote") ? "is-active" : ""}
+            >
+              Blockquote
+            </button>
+          </div>
+        </BubbleMenu>
+      )}
+      <EditorContent editor={editor} />
       <div className="button-group">
         <button
           onClick={() => editor.chain().focus().toggleBold().run()}
@@ -188,6 +308,17 @@ const extensions = [
       keepAttributes: false,
     },
   }),
+  Collaboration.configure({
+    document: ydoc,
+    provider,
+  }),
+  CollaborationCursor.configure({
+    provider: provider,
+    user: {
+      name: "minh tai",
+      color: "#DDF098",
+    },
+  }),
 ];
 
 const content = `
@@ -223,8 +354,8 @@ const content = `
 const AppMT = () => {
   return (
     <EditorProvider
-      slotBefore={<MenuBar />}
       extensions={extensions}
+      slotAfter={<MenuBar />}
       content={content}
     ></EditorProvider>
   );
