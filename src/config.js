f798ef44673944ec12c6c129e665ca78b20ccba4
devduide <minhnguyen20020524@gmail.com>
2026-05-04 14:43:59 +0700

    feat: centralize environment configuration in a new config module and update API endpoint settings

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const API_LOCAL = "https://localhost:7059";
-const API_HOSTING = import.meta.env.VITE_APP_API_URL || "https://notaion.runasp.net";
+const API_HOSTING = "https://notaion.runasp.net";
 
 export default {
   API_LOCAL,
